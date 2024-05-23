2ac1b322886243d89efc90e2b7008babe66d0136
paulodevhenri <paulocordeiro1992@hotmail.com>
2024-05-23 11:19:54 -0300

    Desafio-trilha-java-criandoContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Criando projeto conta banco");
+                      
+       try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Por favor, digite o número da Agência !: ");
+        String agencia = scanner.next();
+        
+        int numConta = 77255471;
+        String msgConta = "Conta: " + numConta;
+        
+        System.out.println("Olá, Obrigado por criar uma conta em nosso banco ");
+        
+        System.out.println("Sua Agência é: " + agencia + ", Conta: " + numConta);
+    
+       }
+        double saldo = 250.50;
+        String mensagem ="E Seu saldo é de: R$ " + saldo; 
+        System.out.println(mensagem);
+        
+        
     }
 }
